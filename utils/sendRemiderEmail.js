3d061e7d57930b5ea45a8c171d503b95b009438e
Rasel Shikder <46960192+raselShikderDev@users.noreply.github.com>
2025-02-21 10:20:33 +0000

    Added all api with reminder email - 1.0.0

--- utils/sendRemiderEmail.js
+++ utils/sendRemiderEmail.js
@@ -1,17 +1,40 @@
-import dayjs from "dayjs"
-import {emailTemplates, generateEmailTemplate} from "./utils/emailTemplate.js"
-import transporter "./config/nodeMailer.js"
+import dayjs from "dayjs";
+import {
+  emailTemplates
+} from "./utils/emailTemplate.js";
+import transporter from "./config/nodeMailer.js";
 
-export const sendReminderEmail = async(to, type, susbcription) =>{
-    if(!to || !type){
-        throw new Error("Missing required parameter");
-    }
+export const sendReminderEmail = async (to, type, subscription) => {
+  if (!to || !type) {
+    throw new Error("Missing required parameter");
+  }
 
-    const template = emailTemplates.find((t)=>t.label === type)
-    if(!template){
-        throw new Error("Invalid template type")
-    }
-    
-    
-}
+  const template = emailTemplates.find((t) => t.label === type);
+  if (!template) {
+    throw new Error("Invalid template type");
+  }
 
+  const mailInfo = {
+    username: subscription.user.name,
+    subscriptionName: subscription.name,
+    renewalDate: dayjs(subscription.renewalDate()).format("MMM, D, YYYY"),
+    planName: subscription.name,
+    paymentMehtod: subscription.paymentMehtod,
+    price: `${subscription.currency} ${subscription.price} ${subscription.frequency}`,
+  };
+
+  const subject = template.generateSubject(mailInfo);
+  const message = template.generateBody(mailInfo);
+
+  const emailOption = {
+    email: process.env.EMAIL_ACCOUNT,
+    to: to,
+    subject: subject,
+    html: message,
+  };
+
+  await transporter.sendMail(emailOption, (err, info) => {
+    if (err) return console.error("Sending reminders email is faild: ", err);
+    console.log(`Reminder email has sent: ${info.message}`);
+  });
+};
